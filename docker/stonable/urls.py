600a435ddb53a105c96e6467457395f7d6c2bf3e
piseb <piseb@tuta.com>
2024-06-03 23:47:47 +0200

    feat form create entite and save model

--- docker/stonable/urls.py
+++ docker/stonable/urls.py
@@ -5,4 +5,7 @@ APP_NAME = "stonable"
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("annuaire", views.annuaire, name="annuaire"),
+    path("annuaire/entite/new", views.entite_create, name="entite_create"),
+    path("annuaire/entite/<int:entite_id>", views.entite_read, name="entite_read"),
 ]
--- docker/stonable/views.py
+++ docker/stonable/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 from django.contrib import messages
+from .forms import EntiteForm
+from .models import Entite
+
 
 def home(request):
     return render(request, "home.html")
+
+
+def annuaire(request):
+    entites = Entite.objects.all()
+    return render(request, "annuaire/index.html", {"entites": entites})
+
+
+def entite_read(request, entite_id):
+    entite = get_object_or_404(Entite, pk=entite_id)
+    return render(request, "annuaire/entite.html", {"entite": entite})
+
+
+def entite_create(request):
+    if request.method == "POST":
+        details = EntiteForm(request.POST)
+        if details.is_valid():
+            details.save()
+            return HttpResponse("data submitted successfully")
+        else:
+            return render(request, "annuaire/entite.html", {"form": details})
+    else:
+        form = EntiteForm()
+        return render(request, "annuaire/entite.html", {"form": form})
