bc5e76d6145244e5bea392b0bbe8c039ae81be4e
piseb <piseb@tuta.com>
2024-06-03 20:04:35 +0200

    fix settings databases engine

--- docker/app/settings.py
+++ docker/app/settings.py
@@ -76,6 +76,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     "default": {
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ["POSTGRES_DB"],
         "USER": os.environ["POSTGRES_USER"],
         "PASSWORD": os.environ["POSTGRES_PASSWORD"],
