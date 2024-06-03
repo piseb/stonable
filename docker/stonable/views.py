b57c1e074719c333af7d337169846f7947704c41
piseb <piseb@tuta.com>
2024-06-03 19:25:01 +0200

    rebase main

--- docker/stonable/views.py
+++ docker/stonable/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
+<<<<<<< HEAD
 
 def index(request):
     return render(request, "index.html")
+=======
+def index():
+    return HttpResponse("futur annuaire")
+>>>>>>> 4f3d444 (adding basics urls and view)
