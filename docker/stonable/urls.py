e633520a84ef887f8af01b08ac956af5a3dd7669
piseb <piseb@tuta.com>
2024-06-07 13:48:32 +0200

    fix urls.py

--- docker/stonable/urls.py
+++ docker/stonable/urls.py
@@ -5,7 +5,6 @@ APP_NAME = "stonable"
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("", views.index, name="index"),
     path("entites", views.entites, name="entites"),
     path("entites/new", views.entite_create, name="entite_create"),
     path("entites/<int:entite_id>", views.entite_read, name="entite_read"),
