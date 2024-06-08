502de6f01989c2db318bd1629c0e9b3ca427f2c1
piseb <piseb@tuta.com>
2024-06-08 16:36:11 +0200

    feat alerte messages

--- docker/stonable/views.py
+++ docker/stonable/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
 
-def home(request):
-    return render(request, "home.html")
+def home(request, alertes=None):
+    context = {"alertes": alertes}
+    return render(request, "home.html", context)
