b918d297add131100331b293328f4e64606100b9
piseb <piseb@tuta.com>
2024-06-08 16:58:11 +0200

    code views.py add dict context

--- docker/stonable/views.py
+++ docker/stonable/views.py
@@ -11,43 +11,40 @@ def home(request):
 
 
 def entites(request):
+    context = {}
     # TODO optimiser le query
     entites = Entite.objects.all()
     paginator = Paginator(entites, 10)
 
-    page = request.GET.get("page")
-    if page:
-        page = int(page)
+    page_current = request.GET.get("page")
+    if page_current:
+        page_current = int(page_current)
     else:
-        page = 1
+        page_current = 1
+    context["page_current"] = page_current
 
-    entites = paginator.get_page(page)
-    pages = paginator.page_range
+    entites = paginator.get_page(page_current)
+    context["entites"] = entites
+
+    context["pages"] = paginator.page_range
 
     if entites.has_next():
-        page_next = page + 1
+        page_next = page_current + 1
     else:
         page_next = None
+    context["page_next"] = page_next
 
     if entites.has_previous():
-        page_previous = page - 1
+        page_previous = page_current - 1
     else:
         page_previous = None
+    context["page_previous"] = page_previous
 
-    return render(
-        request,
-        "entites/entites.html",
-        {
-            "entites": entites,
-            "page_current": page,
-            "page_next": page_next,
-            "page_previous": page_previous,
-            "pages": pages,
-        },
-    )
+    return render(request, "entites/entites.html", context)
 
 
 def entite_create_or_update(request, entite_id=None):
+    context = {"entite_id": entite_id}
     if entite_id:
         instance = get_object_or_404(Entite, pk=entite_id)
     else:
@@ -55,20 +52,17 @@ def entite_create_or_update(request, entite_id=None):
 
     if request.method == "POST":
         details = EntiteForm(request.POST, instance=instance)
+        context["form"] = details
         if details.is_valid():
             details.save()
             return HttpResponse("data submitted successfully")
         else:
-            return render(
-                request,
-                "entites/entite.html",
-                {"form": details, "entite_id": entite_id},
-            )
+            return render(request, context)
     else:
         form = EntiteForm(instance=instance)
-        return render(
-            request, "entites/entite.html", {"form": form, "entite_id": entite_id}
-        )
+        context["form"] = form
+        return render(request, "entites/entite.html", context)
+
 
 
 def entite_delete(request, entite_id):
